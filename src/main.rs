42c00a5639945f7dafb631ba462f3597d24f8660
albertswchung@gmail.com <albertswchung@gmail.com>
2021-08-10 20:44:18 +0100

    Working differentiation with lots of cloning.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@ use std::ops::{Add, Sub, Div, Mul};
 use std::collections::{HashMap};
 use std::hash::{Hash, Hasher};
 
-#[derive(Clone,Eq)]
+#[derive(Clone,Debug)]
 struct Variable {
     name: String,
     data: f32,
     grad: Vec<(Variable, f32)>,
+}
+
+impl Variable {
+    fn new(name: String, data: f32) -> Self {
+        Variable {
+            name: name,
+            data: data,
+            grad: vec![],
+        }
+    }
 }
 
 impl Add for Variable {
@@ -26,6 +36,8 @@ impl PartialEq for Variable {
         self.name == other.name
     }
 }
+
+impl Eq for Variable {}
 
 impl Hash for Variable {
     fn hash<H: Hasher>(&self, state: &mut H) {
@@ -52,7 +64,7 @@ impl Mul for Variable {
         Self {
             name: format!("{} * {}", self.name, other.name),
             data: self.data * other.data,
-            grad: vec![(self, other.data), (other, self.data)],
+            grad: vec![(self.clone(), other.data), (other, self.data)],
         }
     }
 }
@@ -61,31 +73,48 @@ impl Div for Variable {
     type Output = Self;
 
     fn div(self, other: Self) -> Self {
+        let x = self.data;
+        let y = other.data;
+
         Self {
             name: format!("{} / {}", self.name, other.name),
             data: self.data * other.data,
-            grad: vec![(self, 1. / other.data), (other, - self.data / other.data.powf(2.))],
+            grad: vec![(self.clone(), 1. / y), (other, - x / y.powf(2.))],
         }
     }
 }
 
-fn compute_gradients(var: Variable, current_value: f32) -> HashMap<Variable, Vec<Variable>> {
-    let gradients = HashMap::new();
-
+fn compute_gradients(gradients: &mut HashMap<Variable, f32>, var: Variable, current_value: f32) {
     for tup in var.grad {
         let child = tup.0;
+        let child_clone = child.clone();
         let gradient = tup.1;
         let value_to_child = current_value * gradient;
-        gradients.insert(child, value_to_child);
-    }
+        if gradients.contains_key(&child) {
+            let child_grad = gradients.entry(child).or_insert(0.);
+            *child_grad += value_to_child;
+        } else {
+            gradients.insert(child, value_to_child);
+        }
 
+        compute_gradients(gradients, child_clone, value_to_child);
+    }
+}
+
+fn get_gradients(var: Variable) -> HashMap<Variable, f32> {
+    let mut gradients = HashMap::new();
+    compute_gradients(&mut gradients, var, 1.);
     gradients
 }
 
-fn get_gradients(var: Variable) -> HashMap<Variable, Vec<Variable>> {
-    let gradients = HashMap::new();
-}
-
 fn main() {
-    println!("Hello, world!");
+    let a = Variable::new("a".to_string(), 4.);
+    let b = Variable::new("b".to_string(), 3.);
+    let c = a.clone() + b;
+    let d = a.clone() * c;
+    println!("d: {:?}", d);
+
+    let grad_d = get_gradients(d);
+    println!("dd/da: {}", grad_d[&a]);
+
 }
